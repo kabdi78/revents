13f88ca85808fa01be2d96c44d14f0d0e26b5ec6
DESKTOP-1DIIKUN\kabdi <kabdi@cirb.brussels>
2018-06-03 20:31:35 +0200

    End of section 11

--- src/app/common/form/PlaceInput.jsx
+++ src/app/common/form/PlaceInput.jsx
@@ -28,7 +28,7 @@ class PlaceInput extends Component {
     return (
       <Form.Field error={touched && !!error} width={width}>
         <Script
-          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVrHA_wUwsIEMFeXpkc_mtIQpIuDlr2Zw&libraries=places"
+          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyCTN8X_q_xtMYCnacteF4ZQj0RKXodI080&libraries=places"
           onLoad={this.handleScriptLoaded}
         />
         {this.state.scriptLoaded && (
